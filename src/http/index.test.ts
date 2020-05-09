b2a57d8ff7984a8f6c17585228f615ae0eb2bcf8
Nazmi Altun <nazmialtun@windowslive.com>
2020-05-09 16:26:09 +0800

    feat: HttpClient

--- src/http/index.test.ts
+++ src/http/index.test.ts
@@ -1,24 +1,43 @@
 import HttpClient from '.';
 
-const serviceUrl = 'http://localhost/';
+const serviceUrl = 'http://localhost';
 const testClient = new HttpClient(serviceUrl);
 
 describe('http', () => {
-  it('get returns json response', async () => {
-    const mockFetch = jest.fn().mockImplementation(() =>
-      Promise.resolve({
-        json: () =>
-          Promise.resolve({
-            data: '12345',
-          }),
-      })
-    );
-    window.fetch = mockFetch;
-    localStorage.setItem('access_token', 'token-1234');
+  const mockFetch = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      ok: true,
+      text: () => Promise.resolve('Successful!'),
+      json: () =>
+        Promise.resolve({
+          data: '12345',
+        }),
+    })
+  );
+  window.fetch = mockFetch;
+  localStorage.setItem('access_token', 'token-1234');
 
+  it('get returns json response', async () => {
     const response = await testClient.get('api/v1/statistics');
     expect(response.data).toEqual('12345');
     expect(mockFetch.mock.calls[0][0]).toBe(`${serviceUrl}/api/v1/statistics`);
     expect(mockFetch.mock.calls[0][1].headers.Authorization).toBe(`Bearer ${localStorage.getItem('access_token')}`);
+  });
+
+  it('post returns response', async () => {
+    const response = await testClient.post('api/v1/add-sms', {
+      data: 'test',
+    });
+    expect(response.ok).toBeTruthy();
+    expect(await response.text()).toBe('Successful!');
+    expect(mockFetch.mock.calls[1][0]).toBe(`${serviceUrl}/api/v1/add-sms`);
+    expect(mockFetch.mock.calls[1][1].headers.Authorization).toBe(`Bearer ${localStorage.getItem('access_token')}`);
+  });
+
+  it('delete returns response', async () => {
+    const response = await testClient.delete('api/v1/students/1');
+    expect(response).toBe('Successful!');
+    expect(mockFetch.mock.calls[2][0]).toBe(`${serviceUrl}/api/v1/students/1`);
+    expect(mockFetch.mock.calls[2][1].headers.Authorization).toBe(`Bearer ${localStorage.getItem('access_token')}`);
   });
 });
