a6524030b64178e0c4365c570e32dd876ae983e1
Nazmi Altun <nazmialtun@windowslive.com>
2020-05-14 13:27:57 +0800

    fix: skip commonjs build

--- scripts/build.sh
+++
@@ -1,2 +0,0 @@
-yarn run tsc --declaration true --declarationMap true --module esnext --outDir "./dist/es"
-yarn run tsc --declaration true --declarationMap true --module commonjs --outDir "./dist/commonjs"
